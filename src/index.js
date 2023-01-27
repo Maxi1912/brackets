b1655c3afade96c4a337c46e7d8307053b60a9d5
Maksym <maximus.sheva@gmail.com>
2023-01-27 19:48:16 +0200

    feat: implement function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openingBrackets = bracketsConfig.map((brackets) => brackets[0]);
+  const bracketsVocabulary = {};
+  bracketsConfig.forEach((brackets) => {
+    bracketsVocabulary[brackets[1]] = brackets[0];
+  });
+  const specialBrackets = bracketsConfig
+    .filter((brackets) => brackets[0] === brackets[1])
+    .map((brackets) => brackets[0]);
+  let stack = [];
+  let lastItemFromStack;
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+    lastItemFromStack = stack[stack.length - 1];
+    if (specialBrackets.includes(currentBracket)) {
+      lastItemFromStack === currentBracket
+        ? stack.pop()
+        : stack.push(currentBracket);
+    } else {
+      if (openingBrackets.includes(currentBracket)) {
+        stack.push(currentBracket);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+        if (lastItemFromStack === bracketsVocabulary[currentBracket]) {
+          stack.pop();
+        }
+      }
+    }
+  }
+  return stack.length === 0;
+};
